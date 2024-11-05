e10460057c777dc022975c828f9a5f980240fb21
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:51:11 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 
 uploaded_file = st.file_uploader("Add text file !")
+uploaded_file.decode('utf-8')
 if uploaded_file:
     for line in uploaded_file:
         st.write(line)
