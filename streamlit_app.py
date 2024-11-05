fc1bd4385c9c7cb21482395506ab3e6b4151279b
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 13:47:33 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,41 +21,4 @@ if uploaded_file is not None:
     minha_lista = uploaded_file.readlines()
     st.markdown("Base de dados carregada com sucesso!")
 
-    #dados = []
-    for linha in minha_lista:
-        # Remover quebras de linha e espaços em branco (se necessário)
-        linha = linha.strip()
-
-            # Dividindo a linha em partes usando espaços como delimitador
-        partes = linha.split()
-
-        """# Encontrando os índices das palavras-chave
-        indice_conta = partes.index('Conta:') + 1
-        indice_mes_ano = partes.index('Mês/ano') + 1
-        indice_rendimento = partes.index('RENDIMENTO') + 1
-
-        # Extraindo os dados
-        conta = partes[indice_conta]
-        mes_ano = partes[indice_mes_ano]
-        rendimento = partes[indice_rendimento]
-
-        dados.append([conta, mes_ano, rendimento])
-
-        # Criar o DataFrame
-    df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
-    
-    # Create 'mes' and 'ano' columns by splitting 'Mês/ano referência'
-    df[['MÊS', 'ANO']] = df['Mês/ano referência'].str.split('/', expand=True)
-
-    # Remove 'Mês/ano referência' column
-    df = dataframe.drop('Mês/ano referência', axis=1)
-    # Create a copy of the dataframe
-    df_copy = df.copy()
-
-    # Combine the copied dataframe with the original dataframe 
-    combined_df = pd.concat([df, df_copy], axis=0)"""
-
-    if minha_lista.empty:
-        print("DataFrame está vazio.")
-    else:
-        st.data_editor(minha_lista)
+    st.data_editor(minha_lista)
