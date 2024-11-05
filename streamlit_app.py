20610185bddddb62f9dd3998bb7ab9cadd791ddb
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 15:00:59 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,31 @@ uploaded_file = st.file_uploader("Add text file !")
 
 if uploaded_file:
     linhas_limpas = []
+
     for line in uploaded_file:
         texto = line.decode('latin-1')
         # Remove caracteres especiais (ajuste conforme necessário)
         texto = texto.strip()
         linhas_limpas.append(texto)
+        
+        dados = []
+
         for line in texto:
-        # Remover quebras de linha e espaços em branco (se necessário)
-            line = line.strip()
+            linha = linha.strip()
 
-            # Expressões regulares para extrair os dados
-            conta = re.search(r'Conta:\s+(\S+)', linha)
-            mes_ano = re.search(r'Mês/ano referência:\s+(\S+)', linha)
-            rendimento = re.search(r'RENDIMENTO LÍQUIDO\s+(\S+)', linha)
+            # Dividindo a linha em partes usando espaços como delimitador
+            partes = linha.split()
 
-            if conta and mes_ano and rendimento:
-                dados.append([conta.group(1), mes_ano.group(1), rendimento.group(1)])
-            else:
-                print(f"Linha não processada: {linha}")
+            # Encontrando os índices das palavras-chave
+            indice_conta = partes.index('Conta:') + 1
+            indice_mes_ano = partes.index('Mês/ano') + 1
+            indice_rendimento = partes.index('RENDIMENTO') + 1
 
-            # Criar o DataFrame
+            # Extraindo os dados
+            conta = partes[indice_conta]
+            mes_ano = partes[indice_mes_ano]
+            rendimento = partes[indice_rendimento]
+            
+            dados.append([conta, mes_ano, rendimento])
+
             df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
