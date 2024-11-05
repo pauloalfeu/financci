5ce3f7095285244e8965183e5e1f629b2f3234ed
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 13:26:36 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,10 +85,10 @@ if uploaded_file is not None:
     #################### CHAMANDHO AS FUNÇOES ##################################
 
     df = extrair_dados_lista(minha_lista)
-    df = formatar_dados_df(df)
+    dtf = formatar_dados_df(df)
 
 
     if df.empty:
         print("DataFrame está vazio.")
     else:
-        st.data_editor(df)
+        st.data_editor(dtf)
