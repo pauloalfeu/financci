45112b47ed94e98e3d5d31639eaba2c5aa743222
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 10:52:43 -0300

    another-update

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,7 @@ st.title("🎯 Financci")
 ################################### SEÇÃO DE UPLOAD DE DATAFRAME
 st.divider()
 st.markdown("##### Carregue um arquivo _.txt_ clicando em \"Browse files\" no campo abaixo:")
-st.markdown("> **Importante:** siga as etapas apresentadas na guia **Portal da Transparência de Cascavel** para fazer o download do arquivo correto.")
+st.markdown("> **Importante:** siga as etapas apresentadas na guia **Tutorial de upload de arquivos** para fazer o download do arquivo correto.")
 uploaded_file = st.file_uploader("")
 if uploaded_file is not None:
     # Recebendo arquivo.txt:
