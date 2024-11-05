abdcde5c5443f7349130b38e1883b3feb3f3c18c
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:10:47 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,5 @@ import pandas as pd
 uploaded_file = st.file_uploader("Escolha um arquivo .txt", type=["txt"])
 
 if uploaded_file is not None:
-    df = pd.read_csv(uploaded_file, sep='\t')  # Ajustar o separador se necessário
+    df = pd.read_csv(uploaded_file, sep='\s+')  # Ajustar o separador se necessário
     st.dataframe(df)
