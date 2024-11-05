da6a9fc46fad77104bf216f11a314792b3fbcba1
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:53:22 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,7 @@ import streamlit as st
 uploaded_file = st.file_uploader("Add text file !")
 
 if uploaded_file:
+    linhas_limpas = []
     for line in uploaded_file:
         texto = line.decode('utf-8')
         # Remove caracteres especiais (ajuste conforme necessário)
