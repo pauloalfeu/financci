831552e7b823076812c1e2cd4a3ca85540181301
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:57:26 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,4 +10,19 @@ if uploaded_file:
         # Remove caracteres especiais (ajuste conforme necessário)
         texto = texto.strip()
         linhas_limpas.append(texto)
-        st.write(texto)
+        for line in texto:
+        # Remover quebras de linha e espaços em branco (se necessário)
+            line = line.strip()
+
+            # Expressões regulares para extrair os dados
+            conta = re.search(r'Conta:\s+(\S+)', linha)
+            mes_ano = re.search(r'Mês/ano referência:\s+(\S+)', linha)
+            rendimento = re.search(r'RENDIMENTO LÍQUIDO\s+(\S+)', linha)
+
+            if conta and mes_ano and rendimento:
+                dados.append([conta.group(1), mes_ano.group(1), rendimento.group(1)])
+            else:
+                print(f"Linha não processada: {linha}")
+
+            # Criar o DataFrame
+            df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
