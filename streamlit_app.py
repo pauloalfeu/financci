16a6273f26142eaf2eca19abdd86a9f12d7ea5cf
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 15:08:55 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,24 +14,6 @@ if uploaded_file:
         
         dados = []
 
-    """for line in texto:
-        line = line.strip()
 
-        # Dividindo a linha em partes usando espaços como delimitador
-        partes = line.split()
-
-        # Encontrando os índices das palavras-chave
-        indice_conta = partes.index('Conta:\s+(\S+)') + 1
-        indice_mes_ano = partes.index('Mês/ano referência:\s+(\S+)') + 1
-        indice_rendimento = partes.index('RENDIMENTO LÍQUIDO\s+(\S+)') + 1
-
-        # Extraindo os dados
-        conta = partes[indice_conta]
-        mes_ano = partes[indice_mes_ano]
-        rendimento = partes[indice_rendimento]
-            
-        dados.append([conta, mes_ano, rendimento])
-
-        df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])"""
 
     st.write(texto)
