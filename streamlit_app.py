b2393bf09c0b1c17967d85f102fc2dee75a82f86
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:35:56 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,9 +7,9 @@ if uploaded_file:
         partes = line.split()
 
         # Encontrando os índices das palavras-chave
-        indice_conta = partes.index('Conta:') + 1
-        #indice_mes_ano = partes.index('Mês/ano') + 1
-        indice_rendimento = partes.index('RENDIMENTO') + 1
+        indice_conta = partes.index('Conta:\s+(\S+)') + 1
+        indice_mes_ano = partes.index('Mês/ano referência:\s+(\S+)') + 1
+        indice_rendimento = partes.index('RENDIMENTO LÍQUIDO\s+(\S+)') + 1
 
         # Extraindo os dados
         conta = partes[indice_conta]
