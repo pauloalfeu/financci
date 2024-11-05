92d894a1ac14cb9682be25ceb1785b0994d5ad87
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:22:54 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ uploaded_file = st.file_uploader("Add text file !")
 if uploaded_file:
     for line in uploaded_file:
         # Remover quebras de linha e espaços em branco (se necessário)
-        linha = linha.strip()
+        line = line.strip()
 
         # Expressões regulares para extrair os dados
         conta = re.search(r'Conta:\s+(\S+)', linha)
