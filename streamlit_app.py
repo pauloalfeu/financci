b959b07afdb44be8fb7171adf30b68277c74d234
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 16:33:46 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,7 +54,9 @@ if uploaded_file:
             print("A linha 'rendimento' não foi encontrada no DataFrame.")
 
 
+    df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
+    # Adicionando os dados da imagem ao DataFrame
+    for i, valor in enumerate(dados):
+        df.loc[0, df.columns[i]] = valor
     
-    #df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
-    #st.write((type(linhas_limpas)))
-    st.data_editor(dados)
+    st.data_editor(df)
