a37cd703078ab2a76d0bb1e663e2b3189813d619
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 16:24:08 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ if uploaded_file:
 
     # Iterando sobre as linhas do DataFrame
     dados = []
-    df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
+
     
     for index, row in df.iterrows():
         if 'Conta:' in row.values:
@@ -50,11 +50,11 @@ if uploaded_file:
         if 'rendimento' not in locals():
             print("A linha 'rendimento' não foi encontrada no DataFrame.")
 
-            if numero_conta:
-                dados.append(numero_conta)
+            if numero_conta and mes_ano and rendimento:
+                dados.append([numero_conta.group(1), mes_ano.group(1), rendimento.group(1)])
             else:
                 print(f"Linha não processada")
     
-    
+    df = pd.DataFrame(dados, columns=['CONTA', 'Mês/ano referência', 'RENDIMENTO LÍQUIDO'])
     #st.write((type(linhas_limpas)))
     st.data_editor(df)
