2f348dda16a2f0ede0f877a6da027b8b7122bbb9
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 11:17:47 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-from io import StringIO
 from wordcloud import WordCloud
 import PIL.Image as Image
 from collections import Counter
