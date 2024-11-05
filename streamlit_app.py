4ba24a3455f02f25f4b3d8757e4fc311aab4c662
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:07:23 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,8 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
-from collections import Counter
 
+uploaded_file = st.file_uploader("Escolha um arquivo .txt", type=["txt"])
 
-
-st.title("🎯 Financci")
-
-
-################################### SEÇÃO DE UPLOAD DE DATAFRAME
-st.divider()
-st.markdown("##### Carregue um arquivo _.txt_ clicando em \"Browse files\" no campo abaixo:")
-st.markdown("> **Importante:** siga as etapas apresentadas na guia **Tutorial de upload de arquivos** para fazer o download do arquivo correto.")
-uploaded_file = st.file_uploader("")
 if uploaded_file is not None:
-    # Recebendo arquivo.txt:
-    minha_lista = uploaded_file
-    st.markdown("Base de dados carregada com sucesso!")
-
-    st.data_editor(minha_lista)
+    df = pd.read_csv(uploaded_file, sep='\t')  # Ajustar o separador se necessário
+    st.dataframe(df)
