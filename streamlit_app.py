4535b0c6e8adc438b4f4721361b2ebaf2966b0ee
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-05 14:14:56 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ import pandas as pd
 uploaded_file = st.file_uploader("Escolha um arquivo .txt", type=["txt"])
 
 if uploaded_file is not None:
-    minha_lista = uploaded_file.readlines()
+    minha_lista = uploaded_file
     df = pd.read_csv(minha_lista, sep='\s+')  # Ajustar o separador se necessário
     st.dataframe(df)
