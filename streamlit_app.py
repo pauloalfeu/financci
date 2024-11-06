0c3f786f6e53c34be7f266a80227d82296aa9dcc
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-06 09:36:49 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,4 +76,4 @@ if uploaded_files is not None:
 
             st.data_editor(df)
     except Exception as e:
-        st.error("Ocorreu um erro inesperado com o arquivo enviado. Por favor, tente novamente. (ARQUIVO COM ERRO:", uploaded_file.name, ")")
+        st.error("Ocorreu um erro inesperado com o arquivo enviado. Por favor, tente novamente. ARQUIVO COM ERRO:", uploaded_file.name)
