c8930031063c65d44b095bfb95ebfa5b2dbdb38a
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-06 10:41:54 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -77,9 +77,9 @@ if uploaded_files is not None:
 
                 #st.data_editor(df)
                 list_of_dfs.append(df)
+                st.data_editor(list_of_dfs)
             except Exception as e:
                 st.error(f"Ocorreu um erro inesperado com o arquivo: \"{uploaded_file.name}\" presente nos arquivos enviados. Por favor, verifique-o e tente novamente.")
-            st.data_editor(list_of_dfs)
 
 #    except Exception as e:
 #        st.error("Ocorreu um erro inesperado com um dos arquivos enviados. Por favor, tente novamente.")
