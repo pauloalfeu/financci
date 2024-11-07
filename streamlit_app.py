c9c8106669312fc1472edcd9cf72b7273adc7e3b
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-07 11:04:23 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,8 +37,9 @@ Este aplicativo te ajudará a analisar os seus rendimentos bancários de forma r
 st.info("**Problema com o arquivo?** Se você ver uma mensagem de erro, é provável que o arquivo tenha um formato inesperado. Clique no **'X'** para removê-lo e tente novamente.")
 
 st.markdown("""
-**Dúvidas extras?** Entre em contato com o desenvolvedor.""")
-st.divider()
+**Dúvidas extras?** Entre em contato com o desenvolvedor.
+---""")
+#st.divider()
 
 
 
