b36e285085d502978b5340e83c8f259f8738d18c
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-07 16:23:17 -0300

    update name

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ with st.sidebar:
     """)
 
 
-st.markdown("<h1 style='color: rgb(0, 66, 128);'> 📝 Gerenciador de Rendimentos</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='color: rgb(0, 66, 128);'> 📝 Calculador de Rendimentos</h1>", unsafe_allow_html=True)
 st.markdown("""
 
 Este aplicativo te ajudará a analisar os seus rendimentos bancários de forma rápida e eficiente.
