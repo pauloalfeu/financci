563c18b7e2fd085b67a7d8773808dcaa92ec32b0
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-07 11:04:39 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,7 +38,8 @@ st.info("**Problema com o arquivo?** Se você ver uma mensagem de erro, é prov�
 
 st.markdown("""
 **Dúvidas extras?** Entre em contato com o desenvolvedor.
----""")
+---
+""")
 #st.divider()
 
 
