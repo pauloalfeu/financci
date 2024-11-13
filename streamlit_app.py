703b52cf1fc53f932a761c82be39a8626011d45e
pauloalfeu <127105307+pauloalfeu@users.noreply.github.com>
2024-11-13 08:58:06 -0300

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-st.set_page_config(page_title="Calculador de Rendimentos", page_icon = im)
+st.set_page_config(page_title="Calculador de Rendimentos")
 
 with st.sidebar:
     st.title("Instruções")
